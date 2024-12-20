a8d1d20fd195388610eee08b9fb633543d8c6ed5
Twan code his life <nddt1810@gmail.com>
2024-12-20 11:08:55 +0700

    Delete assets directory

--- assets/js/data/swconf.js
+++
@@ -1,1 +0,0 @@
-const swconf = { cacheName: 'chirpy-1734268096',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
